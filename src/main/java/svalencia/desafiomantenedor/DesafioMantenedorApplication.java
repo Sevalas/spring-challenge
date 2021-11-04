4a00ed36bb803d85b253322e7062d63b2866cffb
svalencia <svalencia.accenture@bancochile.cl>
2021-11-04 10:51:28 -0300

    Initial Commit

---
+++ src/main/java/svalencia/desafiomantenedor/DesafioMantenedorApplication.java
@@ -0,0 +1,13 @@
+package svalencia.desafiomantenedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMantenedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMantenedorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/svalencia/desafiomantenedor/DesafioMantenedorApplicationTests.java
@@ -0,0 +1,13 @@
+package svalencia.desafiomantenedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMantenedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
